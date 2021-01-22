62bca0e9e07a3f77902fea0d206303beb457621c
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-01-21 21:45:51 -0500

    Valores personalizados en la cabecera. Envio de datos en el cuerpo del mensaje.

--- code/node-basic/server.js
+++ code/node-basic/server.js
@@ -1,15 +1,28 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 var app = express()
+
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded({extended:false}) )
 app.use( router )
 
-router.get('/', function(req, res) {
-    res.send('Hola desde GET.')
+router.get('/mensaje', function(req, res) {
+    console.log( req.headers )
+    res.header({
+        "custom-header":"Nuestro valor personalizado."
+    })
+    res.send('Lista de mensajes.')
 })
 router.post('/', function(req, res) {
     res.send('Hola desde POST.')
 })
+router.delete('/mensaje', function(req, res) {
+    console.log( req.query )
+    console.log( req.body )
+    res.send( 'Mensaje eliminado correctamente. ' + req.body.text )
+})
 
-app.listen(3000)
-console.log(`La aplicación está escuchando en http://localhost:3000`)
+app.listen(5000)
+console.log(`La aplicación está escuchando en http://localhost:5000`)
