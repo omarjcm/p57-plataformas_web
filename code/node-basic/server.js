1db622f84867da85a62f5f89c8b7070fcf6e27b5
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-01-21 21:15:52 -0500

    Uso de GET y POST.

--- code/node-basic/server.js
+++ code/node-basic/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
+app.use( router )
 
-app.use('/', function(req, res) {
-    res.send('Hola')
+router.get('/', function(req, res) {
+    res.send('Hola desde GET.')
+})
+router.post('/', function(req, res) {
+    res.send('Hola desde POST.')
 })
 
 app.listen(3000)
-
 console.log(`La aplicación está escuchando en http://localhost:3000`)
