3a52b466f0c7f9a9668d82459f741a953987663e
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-01-21 22:13:06 -0500

    COn publicacion de archivos estaticos por parte del cliente.

--- code/node-basic/server.js
+++ code/node-basic/server.js
@@ -11,14 +11,21 @@ app.use( bodyParser.urlencoded({extended:false}) )
 app.use( router )
 
 router.get('/mensaje', function(req, res) {
-    response.success( req, res, 'Lista de mensajes.' )
+    if (req.query.error == 'ok') {
+        response.error(req, res, 'Error simulado.') 
+    } else {
+        response.success(req, res, 'Lista de mensajes.', 201) 
+    }
 })
+
 router.post('/', function(req, res) {
     res.send('Hola desde POST.')
 })
 router.delete('/mensaje', function(req, res) {
     response.success( req, res, 'Eliminado correctamente.' )
 })
+
+app.use('/', express.static('public'))
 
 app.listen(5000)
 console.log(`La aplicación está escuchando en http://localhost:5000`)
