a574e213f330b9bd4f99acec2b192fb94c744840
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-02-11 21:07:50 -0500

    Separando la logica de conexion a la Base de Datos y los datos de configuracion del proyecto.

--- code/node-project/components/carrera/storage.js
+++ code/node-project/components/carrera/storage.js
@@ -1,16 +1,4 @@
-const db = require('mongoose')
 const Model = require('./model')
-
-const uri = "mongodb+srv://ups:12345@cluster0.pjyad.gcp.mongodb.net/ups?retryWrites=true&w=majority";
-
-db.Promise = global.Promise
-db.connect(uri, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    dbName:'ups'
-})
-    .then(() => console.log('[db] Conectada con éxito.'))
-    .catch((error) => console.error('[error] ', error))
 
 function addCarrera( carrera ) {
     const objeto = new Model( carrera )
--- code/node-project/server.js
+++ code/node-project/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+const config = require('./config')
+const db = require('./db')
 const router = require('./network/routes')
+
+db( config.dbUrl )
 
 var app = express()
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded({extended:false}) )
 router(app)
 
-app.use('/', express.static('public'))
-app.listen(3000)
-console.log('La aplicación está escuchando en http://localhost:3000')
+app.use( config.publicRoute , express.static('public'))
+app.listen( config.port ) 
+console.log( `La aplicación está escuchando en ${config.host}:${config.port}${config.publicRoute}` )
